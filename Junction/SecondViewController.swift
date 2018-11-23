ac68204cb230c6533ff63ac6b272f15d58ea7def
mnimants <nimantsm@gmail.com>
2018-11-23 18:51:45 +0200

    Initial Commit

---
+++ Junction/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Junction
+//
+//  Created by Marcis Nimants on 23/11/2018.
+//  Copyright © 2018 Marcis Nimants. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
